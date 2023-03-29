83801ef57913a7ab46243cdcee8bf7df22e14a14
want2say <w8115405@gmail.com>
2023-03-28 20:13:13 -0400

    newtest

---
+++ test_main.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+def test():
+    assert 2 == 2
+    if 2 == 3:
+        raise SystemExit(1)
+
+# def test_mytest():
+#     with pytest.raises(SystemExit):
+#         test()
+
+def testLogin():
+    print("test ture")
